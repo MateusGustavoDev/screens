98dcf8567a44236582d65cb4a44bd93b240736f6
MateusGustavo22 <githubmateus@gmail.com>
2023-12-26 00:41:14 -0300

    feat: add main banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 export const poppins = Poppins({
   weight: ["400", "600", "900"],
@@ -20,7 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable}`}>{children}</body>
+      <body className={`${poppins.variable} bg-base-400`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Banner from "./components/Banner";
+
 export default function Home() {
-  return <h1 className="bg-bran font-poppins flex">Hello World</h1>;
+  return <Banner />;
 }
