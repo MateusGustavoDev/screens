bf02ea0336cd6fa2914678e21876fdfcb293a207
MateusGustavo22 <githubmateus@gmail.com>
2023-12-30 22:33:07 -0300

    chore: add .env in gitignore

--- components/ui/SearchInput.tsx
+++
@@ -1,9 +0,0 @@
-import { Search } from "lucide-react";
-
-export default function SearchInput() {
-  return (
-    <button>
-      <Search size={22} color="white" />
-    </button>
-  );
-}
--- env.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    GOOGLE_CLIENT_ID: string;
-    GOOGLE_CLIENT_SECRET: string;
-  }
-}
