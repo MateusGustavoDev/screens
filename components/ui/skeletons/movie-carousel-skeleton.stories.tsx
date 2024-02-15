512f79485b42e3ba0ff7e3ca05a11c1f24b92d17
MateusGustavo22 <githubmateus@gmail.com>
2024-02-15 01:47:17 -0300

    refactor: skeletonTheme como decorators do MovieCardSkeleton

--- components/ui/skeletons/movie-carousel-skeleton.stories.tsx
+++ components/ui/skeletons/movie-carousel-skeleton.stories.tsx
@@ -1,9 +1,22 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { MovieCardSkeleton } from './movie-carousel-skeleton'
+import { SkeletonTheme } from 'react-loading-skeleton'
 
 const meta: Meta<typeof MovieCardSkeleton> = {
   title: 'ui/MovieCarouselSkeleton',
   component: MovieCardSkeleton,
+  decorators: [
+    (Story) => {
+      return (
+        <SkeletonTheme
+          baseColor="var(--skeleton-base-color)"
+          highlightColor="var(--skeleton-highlight-color)"
+        >
+          {Story()}
+        </SkeletonTheme>
+      )
+    },
+  ],
 }
 
 export default meta
